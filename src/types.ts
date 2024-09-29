2b89af7c1c87e3f4b716df8a098ff5aa85d476b7
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-09-29 11:57:59 +0300

    added monsters

--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,20 @@ export interface Player {
     person: Person;
     initiative: number;
 }
+
+export interface Monster {
+    id: string | number;
+    name: string;
+    description: string;
+    hp: number;
+    initiative: number;
+    moves: MonsterMove[];
+    attack(target: Monster): void;
+    useMove(move: MonsterMove, target: Monster): void;
+}
+
+export interface MonsterMove {
+    name: string;
+    damage: number;
+    description: string;
+}
